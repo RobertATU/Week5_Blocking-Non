ac86a6601a4546f8640ce2e6f6ff316cdb856858
G00395612@atu.ie <G00395612@atu.ie>
2023-10-18 09:31:43 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/week5_blockingnon/Week5BlockingNonApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_blockingnon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5BlockingNonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week5BlockingNonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/week5_blockingnon/Week5BlockingNonApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week5_blockingnon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week5BlockingNonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
