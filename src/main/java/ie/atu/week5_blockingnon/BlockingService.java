0ad2750fe0e97e660332b37e30ab35bab3a9b803
G00395612@atu.ie <G00395612@atu.ie>
2023-10-18 10:24:06 +0100

    Fixed deleted classes

--- src/main/java/ie/atu/week5_blockingnon/BlockingService.java
+++ src/main/java/ie/atu/week5_blockingnon/BlockingService.java
@@ -1,2 +1,17 @@
-package ie.atu.week5_blockingnon;public class BlockingService {
+package ie.atu.week5_blockingnon;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class BlockingService {
+    private final RestTemplate restTemplate;
+    public BlockingService(RestTemplate restTemplate){this.restTemplate = restTemplate;}
+    public String fetchDataBlocking() {
+        try{Thread.sleep(2000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        return restTemplate.getForObject("https://jsonplaceholder.typicode.com/todos",String.class);
+    }
 }
